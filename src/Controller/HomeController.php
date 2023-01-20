01b70fc5a474fe7259091de8d4edd147e9c3fad0
HubrG <hubrgiorgi@gmail.com>
2023-01-20 19:05:07 +0100

    Ajout du fichier .htaccess

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,7 +11,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/{age<\d+>?0}', name: 'app_home')]
     public function index($age, Request $request, LoggerInterface $logger): Response
     {
         return $this->render('home/home.html.twig', [

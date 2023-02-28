5ec349abca2f2eebf14b5aa103e95fe9f7e2e62a
HubrG <hubrgiorgi@gmail.com>
2023-02-28 14:46:21 +0100

    Update popups

--- src/Controller/ResetPasswordController.php
+++ src/Controller/ResetPasswordController.php
@@ -157,7 +157,7 @@ class ResetPasswordController extends AbstractController
         $email = (new TemplatedEmail())
             ->from(new Address('hubert@scrilab.fr', 'Scrilab'))
             ->to($user->getEmail())
-            ->subject('Your password reset request')
+            ->subject('Votre requête de réinitialisation de mot de passe')
             ->htmlTemplate('reset_password/email.html.twig')
             ->context([
                 'resetToken' => $resetToken,

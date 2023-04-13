df82affa60c6836167e46398183d8ca37824fa94
HubrG <hubrgiorgi@gmail.com>
2023-04-13 16:07:06 +0200

    Improve publications show

--- assets/scripts/Publication/PublicationShowOne.js
+++ assets/scripts/Publication/PublicationShowOne.js
@@ -73,8 +73,11 @@ export function PublicationShowOne() {
               addBtn.classList.remove("follow-collection-buttons-ok");
               addBtn.classList.add("follow-collection-buttons");
               //
-              addIcon.classList.add("fa-bookmark");
-              addIcon.classList.remove("fa-bookmark-slash");
+              addIcon.classList.add("fa-duotone", "fa-book-bookmark");
+              addIcon.classList.remove(
+                "fa-kit",
+                "fa-regular-book-bookmark-slash"
+              );
               addTitle.innerHTML = "Ajouter à ma collection";
               addInfo.innerHTML =
                 "Vous pourrez retrouver ce récit dans votre collection.";
@@ -82,8 +85,8 @@ export function PublicationShowOne() {
               addBtn.classList.remove("follow-collection-buttons");
               addBtn.classList.add("follow-collection-buttons-ok");
               //
-              addIcon.classList.remove("fa-bookmark");
-              addIcon.classList.add("fa-bookmark-slash");
+              addIcon.classList.remove("fa-duotone", "fa-book-bookmark");
+              addIcon.classList.add("fa-kit", "fa-regular-book-bookmark-slash");
               addTitle.innerHTML = "Retirer de ma collection";
               addInfo.innerHTML =
                 "Vous ne pourrez plus retrouver ce récit dans votre collection.";

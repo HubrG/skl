6120786154530c4a343edc1ec16374ba17d5c82a
HubrG <hubrgiorgi@gmail.com>
2023-04-24 13:29:30 +0200

    Download epub

--- src/Controller/Publication/ChapterShowController.php
+++ src/Controller/Publication/ChapterShowController.php
@@ -550,7 +550,6 @@ class ChapterShowController extends AbstractController
         // $book->setRights("Copyright and licence information specific for the book."); // As this is generated, this _could_ contain the name or licence information of the user who purchased the book, if needed. If this is used that way, the identifier must also be made unique for the book.
         $book->setSourceURL($this->generateUrl('app_publication_show_one', ['id' => $publication->getId(), 'slug' => $publication->getSlug(), 'nbrShowCom' => 10])); // Sets the link to the book's source on the web.
 
-
         // Insert custom meta data to the book, in this case, Calibre series index information.
         CalibreHelper::setCalibreMetadata($book, "Scrilab Ebook", "5");
 
@@ -566,7 +565,12 @@ class ChapterShowController extends AbstractController
         $chapters = $this->pchRepo->findBy(['publication' => $publication, 'status' => 2], ['order_display' => 'ASC']);
         // On récupère le contenu de chacun d'entre eux
         foreach ($chapters as $chapter) {
-            $content = $content_start . $chapter->getContent() . $bookEnd;
+            $rawContent = $chapter->getContent();
+
+            // Suppression des attributs des balises p, h1, h2, h3, h4 etc.
+            $cleanContent = preg_replace('/(<(p|h1|h2|h3|h4)[^>]*>)/i', '<$2>', $rawContent);
+
+            $content = $content_start . "<h1>" . $chapter->getTitle() . "</h1>\n" . $cleanContent . $bookEnd;
             $book->addChapter($chapter->getTitle(), $chapter->getSlug() . '.html', $content, true, EPub::EXTERNAL_REF_ADD);
         }
 

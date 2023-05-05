a1deb69d79e83e94bac1c186bfe548e483f905ea
HubrG <hubrgiorgi@gmail.com>
2023-05-05 16:45:51 +0200

    Forum

--- src/Form/ForumTopicType.php
+++ src/Form/ForumTopicType.php
@@ -5,15 +5,32 @@ namespace App\Form;
 use App\Entity\ForumTopic;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ForumTopicType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', null, ['label' => 'Titre du sujet'])
-            ->add('content', null, ['label' => 'Contenu du sujet']);
+            ->add('title', TextType::class, [
+                'label' => 'Titre du sujet',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez écrire un titre.',
+                    ]),
+                ],
+            ])
+            ->add('content', TextareaType::class, [
+                'label' => 'Contenu du sujet',
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez écrire un message.',
+                    ]),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void

56dd20e5ac626dd7d078f28a2a3395757660d96a
HubrG <hubrgiorgi@gmail.com>
2023-06-01 16:31:14 +0200

    Feed started

--- assets/scripts/Notification.js
+++ assets/scripts/Notification.js
@@ -1,7 +1,11 @@
 let intervalId;
-
+import { NotyDisplay } from "./Noty";
+let firstLoad = true;
 let nbrNotif = 0;
 let nbrMessage = 0;
+let audio = new Audio("/sons/notif.mp3");
+let soundEnabled = false;
+
 export function Notification() {
   if (!document.getElementById("username_login")) {
     return;
@@ -10,6 +14,26 @@ export function Notification() {
   refreshInbox(titled);
   refreshNotif(titled);
   interval(titled);
+  // ! Activation du son
+  document
+    .getElementById("enable-sound")
+    .addEventListener("click", function () {
+      console.log("ok");
+      // Tenter de jouer le son pour obtenir l'autorisation de l'utilisateur
+      let playPromise = audio.play();
+
+      if (playPromise !== undefined) {
+        playPromise
+          .then(function () {
+            // L'autorisation a été accordée
+            soundEnabled = true;
+          })
+          .catch(function (error) {
+            // L'autorisation a été refusée
+            console.log("Lecture automatique du son refusée");
+          });
+      }
+    });
 }
 function interval(titled) {
   if (intervalId) {
@@ -47,11 +71,23 @@ function refreshInbox(titled) {
 function refreshNotif(titled) {
   let newTitle;
   if (document.getElementById("notification-menu-button")) {
+    let nbrNotifold = nbrNotif;
     nbrNotif = parseInt(
       document
         .getElementById("notification-menu-button")
         .getAttribute("data-nbr") || 0
     );
+    if (!firstLoad && nbrNotif > nbrNotifold) {
+      NotyDisplay(
+        '<i class="fa-duotone fa-bells"></i><br>' +
+          document.getElementById("last-notif").textContent,
+        "warning",
+        3000
+      );
+      audio.play(); // play the sound
+    }
+    firstLoad = false;
+
     let totalNbr = nbrMessage + nbrNotif;
     newTitle = totalNbr > 0 ? "(" + totalNbr + ") " + titled : titled;
     document.title = newTitle;
